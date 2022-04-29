d823e243a98b20821f7fb51fd4f217d92c46dbdc
Optimus-alfaomega <a-and-o-technologies@outlook.com>
2022-04-29 09:19:31 -0500

    first commit

---
+++ 2-suma consecutiva total/total.c
@@ -0,0 +1,30 @@
+/***************************************************
+ * total -- Demostracion de control flujo          *
+ *                                                 *
+ ---------------------------------------------------
+ * break                                           *
+ **************************************************/
+ #include <stdio.h>
+
+ char linea[100]; /* Linea para datos de entrada */
+ int total;       /* El total de todos los numeros */
+ int item;        /* el proximo item para add a la lista */
+
+int main(){
+ total = 0;
+ while(1){
+   printf("Ingrese el #  a agregar\n");
+   printf("o 0 para detener: ");
+
+   fgets(linea, sizeof(linea), stdin);
+   sscanf(linea, "%d", &item);
+
+   if(item == 0) break;
+
+   total += item;
+   printf("total: %d\n",total);
+ }
+  printf("Total final %d\n", total);
+  return (0);
+
+}
---
+++ README.txt
@@ -0,0 +1,20 @@
+Ejemplo matematicas con sumas simples y sumas consecutivas hasta
+tener un resultado
+
+----------- -
+Hola Mundo en C++
+Libro de referencia "Practical C Programming"
+
+-------------------------------------------------------------------------
+* Para compilar con GCC: *
+
+  $ gcc -g -Wall -o nombre nombre.c
+
+-g:activa el debugging
+-Wall: activa las advertencias
+-o name: 'name' sera el nombre de salida
+nombre.c: es el archivo con el codigo fuente
+
+* Para ejecutar en UNIX like *
+
+  $ ./nombre
